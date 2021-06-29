d6e6222314e686dde2112cac3b03ea9782baaded
anuj26298 <kumar26298anuj@gmail.com>
2021-06-29 09:40:57 +0530

    ADDED code for DayOfMonth problem

---
+++ DayOfMonth.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+var date = prompt("Enter day: ");
+var month = prompt("Enter month: ");
+
+if(month <=6 & date <=20){
+    console.log(true);
+}
+else{
+    if(month >=3 & date >= 20){
+    console.log(true)
+   }
+    else{
+        console.log(false)
+    }
+}
